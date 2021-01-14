501b2ecf5097a9b417376d0210d7cefac53b9d3c
Wandrille Hubert <wahube@microsoft.com>
2021-01-14 15:00:50 -0600

    update migration

--- src/CollectorSqlDb/Migrations/20210114205940_UpdatedMigration.Designer.cs
+++ src/CollectorSqlDb/Migrations/20210114205940_UpdatedMigration.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SqlCollectorDb.Data;
 
 namespace SqlCollectorDb.Migrations
 {
     [DbContext(typeof(SqlCollectorDbContext))]
-    partial class SqlCollectorDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210114205940_UpdatedMigration")]
+    partial class UpdatedMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/CollectorSqlDb/Migrations/20210114205940_UpdatedMigration.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SqlCollectorDb.Migrations
+{
+    public partial class UpdatedMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SqlResource",
+                schema: "app",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ResourceId = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Name = table.Column<string>(type: "varchar(255)", unicode: false, maxLength: 255, nullable: false),
+                    AdminLogin = table.Column<string>(type: "varchar(255)", unicode: false, maxLength: 255, nullable: true),
+                    Type = table.Column<string>(type: "varchar(255)", unicode: false, maxLength: 255, nullable: true),
+                    CreatedOn = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    LastSeenOn = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SqlResource", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SqlResourceDatabase",
+                schema: "app",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ServerNameId = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Name = table.Column<string>(type: "varchar(255)", unicode: false, maxLength: 255, nullable: false),
+                    ServerName = table.Column<string>(type: "varchar(255)", unicode: false, maxLength: 255, nullable: true),
+                    SubscriptionId = table.Column<string>(type: "varchar(255)", unicode: false, maxLength: 255, nullable: true),
+                    CreatedOn = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    LastSeenOn = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SqlResourceDatabase", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SqlResourceDatabaseHistory",
+                schema: "history",
+                columns: table => new
+                {
+                    HistoryID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ID = table.Column<int>(type: "int", nullable: false),
+                    ServerNameId = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Name = table.Column<string>(type: "varchar(255)", unicode: false, maxLength: 255, nullable: false),
+                    ServerName = table.Column<string>(type: "varchar(255)", unicode: false, maxLength: 255, nullable: true),
+                    SubscriptionId = table.Column<string>(type: "varchar(255)", unicode: false, maxLength: 255, nullable: true),
+                    CreatedOn = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    LastSeenOn = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ArchivedOn = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "(sysutcdatetime())")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SqlResourceDatabase", x => x.HistoryID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SqlResourceDatabaseStage",
+                schema: "app",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ServerNameId = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Name = table.Column<string>(type: "varchar(255)", unicode: false, maxLength: 255, nullable: false),
+                    ServerName = table.Column<string>(type: "varchar(255)", unicode: false, maxLength: 255, nullable: true),
+                    SubscriptionId = table.Column<string>(type: "varchar(255)", unicode: false, maxLength: 255, nullable: true),
+                    CreatedOn = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    LastSeenOn = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SqlResourceDatabaseStage", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SqlResourceHistory",
+                schema: "history",
+                columns: table => new
+                {
+                    HistoryID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ID = table.Column<int>(type: "int", nullable: false),
+                    ResourceId = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Name = table.Column<string>(type: "varchar(255)", unicode: false, maxLength: 255, nullable: false),
+                    AdminLogin = table.Column<string>(type: "varchar(255)", unicode: false, maxLength: 255, nullable: true),
+                    Type = table.Column<string>(type: "varchar(255)", unicode: false, maxLength: 255, nullable: true),
+                    CreatedOn = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    LastSeenOn = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ArchivedOn = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "(sysutcdatetime())")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SqlResource", x => x.HistoryID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SqlResourceStage",
+                schema: "app",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ResourceId = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Name = table.Column<string>(type: "varchar(255)", unicode: false, maxLength: 255, nullable: false),
+                    AdminLogin = table.Column<string>(type: "varchar(255)", unicode: false, maxLength: 255, nullable: true),
+                    Type = table.Column<string>(type: "varchar(255)", unicode: false, maxLength: 255, nullable: true),
+                    CreatedOn = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    LastSeenOn = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SqlResourceStage", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SqlResource",
+                schema: "app");
+
+            migrationBuilder.DropTable(
+                name: "SqlResourceDatabase",
+                schema: "app");
+
+            migrationBuilder.DropTable(
+                name: "SqlResourceDatabaseHistory",
+                schema: "history");
+
+            migrationBuilder.DropTable(
+                name: "SqlResourceDatabaseStage",
+                schema: "app");
+
+            migrationBuilder.DropTable(
+                name: "SqlResourceHistory",
+                schema: "history");
+
+            migrationBuilder.DropTable(
+                name: "SqlResourceStage",
+                schema: "app");
+        }
+    }
+}
